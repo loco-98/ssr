8583220828fc0adae6135966fb6f46646cc79742
45711 <45711@192.168.0.126>
2019-06-25 14:58:40 +0800

    first commit

---
+++ ww/src/cn/itcast/servlet/AServlet.java
@@ -0,0 +1,24 @@
+package cn.itcast.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class AServlet extends BaseServlet {
+	public String add(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("add()...");
+		String name = request.getParameter("username");
+		System.out.println("name: " + name);
+		return "f:/index.jsp";
+	}
+	public String update(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("update()...");
+		return "r:/index.jsp";
+	}
+
+}
